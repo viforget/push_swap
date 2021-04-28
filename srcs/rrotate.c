ff78c4ffe612d7df2eb40380bde7236fba62b1a6
Victor Forget <viforget@student.s19.be>
2021-04-28 15:56:03 +0200

    reverse rotate

--- srcs/rrotate.c
+++ srcs/rrotate.c
@@ -6,7 +6,7 @@
 /*   By: viforget <viforget@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/28 15:24:47 by viforget          #+#    #+#             */
-/*   Updated: 2021/04/28 15:24:49 by viforget         ###   ########.fr       */
+/*   Updated: 2021/04/28 15:38:42 by viforget         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,18 @@ t_stacks	rra(t_stacks stacks)
 	t_stack *buf;
 	t_stack *buf2;
 
+	if (!stacks.a)
+		return (stacks);
 	buf = stacks.a;
-	buf2 = buf;
-	stacks.a = stacks.a->next;
+	buf2 = buf->next;
 	while(buf2->next)
-		buf2 = buf2->next;
-	buf2->next = buf;
+	{
+		buf = buf->next;
+		buf2 = buf->next;
+	}
 	buf->next = NULL;
+	buf2->next = stacks.a;
+	stacks.a = buf2;
 	return (stacks);
 }
 
@@ -32,19 +37,24 @@ t_stacks	rrb(t_stacks stacks)
 	t_stack *buf;
 	t_stack *buf2;
 
+	if (!stacks.b)
+		return (stacks);
 	buf = stacks.b;
-	buf2 = buf;
-	stacks.b = stacks.b->next;
+	buf2 = buf->next;
 	while(buf2->next)
-		buf2 = buf2->next;
-	buf2->next = buf;
+	{
+		buf = buf->next;
+		buf2 = buf->next;
+	}
 	buf->next = NULL;
+	buf2->next = stacks.a;
+	stacks.b = buf2;
 	return (stacks);
 }
 
 t_stacks	rrr(t_stacks stacks)
 {
-	stacks = ra(stacks);
-	stacks = rb(stacks);
+	stacks = rra(stacks);
+	stacks = rrb(stacks);
 	return (stacks);
 }
