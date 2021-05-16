0e8cb285ead4f1a71a3551a536c071b9cdf5b7ce
Victor Forget <viforget@student.s19.be>
2021-05-16 17:34:50 +0200

    optimise algo 4

--- srcs/algo/a4.c
+++ srcs/algo/a4.c
@@ -6,7 +6,7 @@
 /*   By: viforget <viforget@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/05 13:42:46 by lobertin          #+#    #+#             */
-/*   Updated: 2021/05/14 18:26:27 by viforget         ###   ########.fr       */
+/*   Updated: 2021/05/16 17:03:02 by viforget         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,6 +40,35 @@ t_stacks	rotate_until(t_stacks stacks, int nb)
 	return (stacks);
 }
 
+int		get_a(t_stack *st, int nb)
+{
+	int cnt;
+	int a;
+	int b;
+
+	a = -1;
+	b = 0;
+	cnt = 1;
+	if (!st)
+		return (0);
+	while(st && st->next)
+	{
+		cnt++;
+		if (st->nb < st->next->nb)
+			b = cnt - 1;
+		if (nb < st->nb && nb > st->next->nb)
+			a = cnt - 1;
+		st = st->next;
+	}
+	if (st->nb < st->nb)
+		b = cnt;
+	if (nb < st->nb && nb > st->nb)
+		a = 0;
+	if (a == -1)
+		return (b);
+	return (a);
+}
+
 t_stacks	scroll_b(t_stacks stacks, int nb)
 {
 	t_stack	*buf;
@@ -64,8 +93,9 @@ t_stacks	scroll_b(t_stacks stacks, int nb)
 		b = cnt;
 	if (nb < buf->nb && nb > stacks.b->nb)
 		a = 0;
-	//printf("b %d %d %d\n", cnt, a == -1 ? b : a, nb);
-	//print_list(stacks.b, "BB");
+	//printf("B %d %d\n", a, b);
+	//print_list(stacks.a, "bA");
+	//print_list(stacks.b, "bB");
 	if (a == -1)
 	{
 		if (b * 2 < (cnt * 2) / 2)
@@ -80,34 +110,6 @@ t_stacks	scroll_b(t_stacks stacks, int nb)
 	return (stacks);
 }
 
-int		get_a(t_stack *st, int nb)
-{
-	int cnt;
-	int a;
-	int b;
-
-	a = -1;
-	b = 0;
-	cnt = 0;
-	if (!st)
-		return (0);
-	while(st && st->next)
-	{
-		cnt++;
-		if (st->nb < st->next->nb)
-			b = cnt - 1;
-		if (nb < st->nb && nb > st->next->nb)
-			a = cnt - 1;
-		st = st->next;
-	}
-	if (st->nb < st->nb)
-		b = cnt;
-	if (nb < st->nb && nb > st->nb)
-		a = 0;
-	if (a == -1)
-		return (b);
-	return (a);
-}
 
 t_stacks	scroll_a(t_stacks stacks, int x, int dir)
 {
@@ -121,9 +123,10 @@ t_stacks	scroll_a(t_stacks stacks, int x, int dir)
 		nb = nb_in_list(stacks.a, sizeoflist(stacks.a) - x);
 	a = get_a(stacks.b, nb);
 	cnt = sizeoflist(stacks.b);
-	//printf("a %d %d %d %d\n", cnt, a, x, nb);
-	//print_list(stacks.a, "AA");
-	//print_list(stacks.b, "BA");
+	//printf("A %d %d %d %d %d\n", dir, x, nb, cnt, a);
+	///print_list(stacks.a, "aA");
+	//print_list(stacks.b, "aB");
+	//sleep(1);
 	while(x--)
 	{
 		if (dir == 1)
@@ -166,7 +169,6 @@ int	hold_first(t_stack *st, int min, int max)
 	int	x;
 
 	x = 0;
-	//printf("%d %d\n", min, max);
 	while (st && (st->nb < min || st->nb > max))
 	{
 		x++;
@@ -195,26 +197,19 @@ t_stacks	a4(t_stacks stacks)
 	tab = lst_to_tab(stacks.a, 1);
 	i = 0;
 	j = 0;
-	while (i <= 11)
+	while (i <= CH)
 	{
-		while (j < sizea / 11 * (i + 1))
+		while (j < sizea / CH * (i + 1) && stacks.a)
 		{
-			x = hold_first(stacks.a, tab[(sizea / 11) * i], tab[(sizea / 11) * (i + 1)]);
-			x2 = hold_second(stacks.a, tab[(sizea / 11) * i], tab[(sizea / 11) * (i + 1)]);
+			x = hold_first(stacks.a, tab[(sizea / CH) * i], tab[(sizea / CH) * (i + 1)]);
+			x2 = hold_second(stacks.a, tab[(sizea / CH) * i], tab[(sizea / CH) * (i + 1)]);
 			if (x2 > x)
 				stacks = scroll_a(stacks, x, 1);
-				//while (x--)
-				//	stacks = print_op("ra", ra, stacks);
 			else
 				stacks = scroll_a(stacks, x2, 2);
-				//while (x2--)
-				//	stacks = print_op("rra", rra, stacks);
 			if (stacks.b && stacks.a)
-			{
 				stacks = scroll_b(stacks, stacks.a->nb);
-			}
-
-			if (stacks.a) //PAS NECESSAIRE
+			if (stacks.a)
 				stacks = print_op("pb", pb, stacks);
 			j++;
 		}
